aab72a4662262d6395b26026fc41c38959f7074e
AlisonD <itsbasictome@gmail.com>
2020-10-12 03:54:33 -0400

    added audio to splash activity

--- app/src/main/java/com/example/cps731lab05sec01alisonder/Splash.java
+++ app/src/main/java/com/example/cps731lab05sec01alisonder/Splash.java
@@ -3,25 +3,31 @@ package com.example.cps731lab05sec01alisonder;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.media.MediaPlayer;
 import android.os.Bundle;
 
 import java.util.Timer;
 import java.util.TimerTask;
 
 public class Splash extends AppCompatActivity {
+    MediaPlayer cafe;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash);
-        TimerTask task=new TimerTask() {
+        cafe = new MediaPlayer();
+        cafe = MediaPlayer.create(this, R.raw.db);
+        cafe.start();
+        TimerTask task = new TimerTask() {
             @Override
             public void run() {
                 finish();
                 startActivity(new Intent(Splash.this, ItemListActivity.class));
-
+                cafe.stop();
             }
         };
-        Timer opening= new Timer();
-        opening.schedule(task,5000);
+        Timer opening = new Timer();
+        opening.schedule(task, 5000);
     }
 }
