291952010631e4eb14d4eff72169ffb7f8b223b0
AlisonD <itsbasictome@gmail.com>
2020-10-12 00:51:46 -0400

    added items to hash map in DummyContent and fixed layout of items in item_list_content

--- app/src/main/java/com/example/cps731lab05sec01alisonder/ItemDetailFragment.java
+++ app/src/main/java/com/example/cps731lab05sec01alisonder/ItemDetailFragment.java
@@ -65,9 +65,9 @@ public class ItemDetailFragment extends Fragment {
         View rootView = inflater.inflate(R.layout.item_detail, container, false);
 
         // Show the dummy content as text in a TextView.
-        if (mItem != null) {
+        /*if (mItem != null) {
             ((TextView) rootView.findViewById(R.id.item_detail)).setText(mItem.details);
-        }
+        }*/
 
         return rootView;
     }
--- app/src/main/java/com/example/cps731lab05sec01alisonder/dummy/DummyContent.java
+++ app/src/main/java/com/example/cps731lab05sec01alisonder/dummy/DummyContent.java
@@ -25,31 +25,21 @@ public class DummyContent {
      */
     public static final Map<String, DummyItem> ITEM_MAP = new HashMap<>();
 
-    private static final int COUNT = 25;
-
     static {
         // Add some sample items.
-        for (int i = 1; i <= COUNT; i++) {
-            addItem(createDummyItem(i));
-        }
+        addItem(new DummyItem("Iced Coffee", "\nWant a nice refreshing drink that involves ice and coffee? This recipe is for you! It is quick, easy, budget-friendly, and tastes just like the gourmet coffee-shops, if not better!"));
+        addItem(new DummyItem("French Vanilla", "\nTired of hot chocolate? Look no more! This is the BEST french vanilla recipe that you will ever find in your entire life."));
+        addItem(new DummyItem("Earl Grey Tea", "\nHave you ever tried milk with tea? This recipe is PERFECT. Just add milk, earl grey tea bag, water, and you are good to go!"));
+        addItem(new DummyItem("Lemonade", "\nLemonade! Clap Clap Clap! Crunchy ice! Clap Clap Clap! Sip it once! Clap Clap Clap! Sip it twice! Clap Clap Clap! Turn around, touch the ground, FREEZE! Enjoy this delicious lemonade."));
+        addItem(new DummyItem("Strawberry Milkshake", "\nStrawberry galore! Get your milk, strawberries (you will need a lot), ice, ICE CREAM and get ready to devour this yummy treat!"));
+        addItem(new DummyItem("Hot Chocolate", "\nTired of french vanilla? Look no more! This is the BEST hot chocolate recipe that you will ever find in your entire life."));
+        addItem(new DummyItem("Ice Cold Water", "\nNeed something to hydrate yourself? Well you are in luck. This is the best ice cold water recipe for YOU! Just pour yourself a glass of water, add some ice cubes, and you are done!"));
+        addItem(new DummyItem("Orange Juice", "\nOrange + juice = OJ! Fresh organic orange juice. Perfect for kids."));
     }
 
     private static void addItem(DummyItem item) {
         ITEMS.add(item);
         ITEM_MAP.put(item.id, item);
-    }
-
-    private static DummyItem createDummyItem(int position) {
-        return new DummyItem(String.valueOf(position), "Item " + position, makeDetails(position));
-    }
-
-    private static String makeDetails(int position) {
-        StringBuilder builder = new StringBuilder();
-        builder.append("Details about Item: ").append(position);
-        for (int i = 0; i < position; i++) {
-            builder.append("\nMore details information here.");
-        }
-        return builder.toString();
     }
 
     /**
@@ -58,12 +48,10 @@ public class DummyContent {
     public static class DummyItem {
         public final String id;
         public final String content;
-        public final String details;
 
-        public DummyItem(String id, String content, String details) {
+        public DummyItem(String id, String content) {
             this.id = id;
             this.content = content;
-            this.details = details;
         }
 
         @NonNull
