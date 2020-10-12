36467a52fe876cfd52cea6244d5a4b30d3050601
AlisonD <itsbasictome@gmail.com>
2020-10-12 02:59:59 -0400

    added image and text to splash layout

--- app/src/main/java/com/example/cps731lab05sec01alisonder/Splash.java
+++ app/src/main/java/com/example/cps731lab05sec01alisonder/Splash.java
@@ -1,5 +1,27 @@
 package com.example.cps731lab05sec01alisonder;
 
-public class Splash {
-    
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+
+import java.util.Timer;
+import java.util.TimerTask;
+
+public class Splash extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+        TimerTask task=new TimerTask() {
+            @Override
+            public void run() {
+                finish();
+                startActivity(new Intent(Splash.this, ItemListActivity.class));
+
+            }
+        };
+        Timer opening= new Timer();
+        opening.schedule(task,5000);
+    }
 }
